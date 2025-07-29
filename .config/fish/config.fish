7e15ee89e5604ab1a312f65424d123a64bd55e29
WarriorGW <ferguerrero.ordonez@gmail.com>
2025-07-29 01:44:18 -0600

    Fish: New git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,11 @@ alias cls="clear"
 alias ls="ls -A --color=auto"
 alias tree="tree -a -I '.git'"
 
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+alias gpl='git pull'
+
 starship init fish | source
