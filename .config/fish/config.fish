230ce9e59d156f39c79370859894a77de5cf4014
WarriorGW <ferguerrero.ordonez@gmail.com>
2025-12-23 19:12:08 -0600

    Added android SDKs and git pull

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,17 @@ alias ga='git add'
 alias gaa='git add .'
 alias gc='git commit -m'
 alias gp='git push'
+alias gf='git fetch'
 alias gpl='git pull'
 
 starship init fish | source
+
+set -gx ANDROID_HOME $HOME/Android/Sdk
+set -gx PATH $ANDROID_HOME/emulator $PATH
+set -gx PATH $ANDROID_HOME/platform-tools $PATH
+set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH
+set -gx PATH $ANDROID_HOME/build-tools/34.0.0 $PATH
+
+function fish_greeting
+  fastfetch
+end
