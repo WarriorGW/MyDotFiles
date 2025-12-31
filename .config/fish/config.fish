72e07f265ea45015a9b8492d2b81dfd8fe620eef
WarriorGW <ferguerrero.ordonez@gmail.com>
2025-12-31 17:40:35 -0600

    fish aliases for better ls with eza and some qol

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 alias cls="clear"
-alias ls="ls -A --color=always --group-directories-first"
+# alias ls="ls -A --color=always --group-directories-first"
 alias tree="tree -a -I '.git'"
 
 alias gs='git status'
@@ -10,7 +10,16 @@ alias gp='git push'
 alias gf='git fetch'
 alias gpl='git pull'
 
+alias ls='eza -a --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+
+alias cmdhist='history | awk '\''{print $1}'\'' | sort | uniq -c | sort -rn | head -10'
+alias rankcmdhist='history | awk '\''{CMD[$1]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}'\'' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10'
+alias zhist='zoxide query -l -s'
+
 starship init fish | source
+zoxide init --cmd cd fish | source
+fzf --fish | source
 
 set -gx ANDROID_HOME $HOME/Android/Sdk
 set -gx PATH $ANDROID_HOME/emulator $PATH
